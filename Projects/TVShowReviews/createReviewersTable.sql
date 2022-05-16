ad4372eb1c138dbf6a4feffa67d998c85a8d9fab
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-17 01:49:43 +0200

    Update Projects/TVShowReviews/createReviewersTable.sql

--- Projects/TVShowReviews/createReviewersTable.sql
+++ Projects/TVShowReviews/createReviewersTable.sql
@@ -1,3 +1,6 @@
-CREATE DATABASE tv_review;
-
-USE tv_review;
+CREATE TABLE reviewers(
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(50) DEFAULT 'UNKNOWN',
+    last_name VARCHAR(50) DEFAULT 'UNKNOWN',
+    PRIMARY KEY(id)
+);
