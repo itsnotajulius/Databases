b4674907bbc52bda246e190958fffee7fa523feb
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-17 01:50:06 +0200

    Update Code/ExampleCode.sql

--- Code/ExampleCode.sql
+++ Code/ExampleCode.sql
@@ -718,7 +718,13 @@ ORDER BY average DESC;
 
 
 
-
+CREATE TABLE series(
+    id INT AUTO_INCREMENT,
+    title VARCHAR(100) DEFAULT 'UNKNOWN',
+    released_year YEAR(4) ,
+    genre VARCHAR(100) DEFAULT 'ANY',
+    PRIMARY KEY(id)
+);
 
 
 -- Showing all individual rating for series 
