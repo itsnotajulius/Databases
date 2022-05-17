e5953dde419355d742cab933cfe9759cec351840
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-17 16:49:29 +0200

    Update Projects/IGClone/createTables.sql

--- Projects/IGClone/createTables.sql
+++ Projects/IGClone/createTables.sql
@@ -27,6 +27,17 @@ CREATE TABLE comments (
 );
 
 
+-- Making the table for likes, needs to have a PRIMARY KEY of forgeign keys for uniqueness.(Could probs use UNIQUE also)
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
+
 
 
 
