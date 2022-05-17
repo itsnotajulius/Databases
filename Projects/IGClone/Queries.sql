ad63c571e9f7ea922393842eeebbd0eab02e7197
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-18 00:00:03 +0200

    Update Projects/IGClone/Queries.sql

--- Projects/IGClone/Queries.sql
+++ Projects/IGClone/Queries.sql
@@ -62,3 +62,12 @@ SELECT  (SELECT Count(*) FROM   photos) /
         AS avg; 
  
 
+-- Top 5 Hashtags
+SELECT tags.tag_name, Count(*) AS total 
+FROM photo_tags 
+JOIN tags 
+    ON photo_tags.tag_id = tags.id 
+GROUP BY tags.id 
+ORDER BY total DESC 
+LIMIT 5; 
+
