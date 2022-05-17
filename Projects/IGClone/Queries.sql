59fb734aa09c124ddb80bcdf76ffba3a5a5b8990
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-17 23:18:47 +0200

    Update Projects/IGClone/Queries.sql

--- Projects/IGClone/Queries.sql
+++ Projects/IGClone/Queries.sql
@@ -6,8 +6,8 @@ LIMIT 5;
 
 
 -- Day most users join query
-SELECT DAYNAME(created_at) AS DAYOFWEEK, COUNT(created_at) AS Amount
+SELECT DAYNAME(created_at) AS DAYOFWEEK, COUNT(created_at) AS Total
 FROM users
 GROUP BY DAYOFWEEK
-ORDER BY DAYOFWEEK
+ORDER BY Total DESC
 LIMIT 1;
