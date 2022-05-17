77740270751239680583c16d5132d00bbe796acd
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-17 23:44:35 +0200

    Update Projects/IGClone/Queries.sql

--- Projects/IGClone/Queries.sql
+++ Projects/IGClone/Queries.sql
@@ -40,6 +40,20 @@ ORDER BY MOSTest DESC
 LIMIT 1;
 
 -- Most liked photo and user who created it
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
+
 
 
  
