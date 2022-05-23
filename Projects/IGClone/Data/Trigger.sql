654c000bef6731482cadd6539f3181088935fe14
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-24 00:22:45 +0200

    Update Projects/IGClone/Data/Trigger.sql

--- Projects/IGClone/Data/Trigger.sql
+++ Projects/IGClone/Data/Trigger.sql
@@ -14,7 +14,17 @@ $$
 DELIMITER ;
 
 
+/* This or the bottom one
+DELIMITER $$
 
+CREATE TRIGGER create_unfollow
+    AFTER DELETE ON follows FOR EACH ROW 
+BEGIN
+    INSERT INTO unfollows(follower_id,followee_id) VALUES(OLD.follower_id,OLD.followee_id);
+END$$
+
+DELIMITER ;
+*/
 
 DELIMITER $$
 
