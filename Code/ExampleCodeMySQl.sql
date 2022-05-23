9dcb91b69b3c19b59b24991266eaa5f6a342ccb4
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-24 00:18:30 +0200

    Update Code/ExampleCodeMySQl.sql

--- Code/ExampleCodeMySQl.sql
+++ Code/ExampleCodeMySQl.sql
@@ -899,6 +899,33 @@ GROUP BY users.id
 HAVING timess=0 OR count=(SELECT COUNT(*) FROM photos)) AS total;
 
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------
 
+CREATE DATABASE trigger_demo;
+
+USE trigger_demo;
+
+CREATE TABLE user(
+    user VARCHAR(100)  NOT NUll DEFAULT "Steve",
+    age INT
+);
+
+ALTER TABLE user RENAME users; 
+
+INSERT INTO users(user, age) VALUES("Matthew",26), ("Rose",17); -- Will work
+
+
+source users_trigger.sql
+
+/*
+DROP TRIGGER users.must_be_adult;
+*/
+
+
+INSERT INTO users(user, age) VALUES("Bruce",27), ("Clark",16); --Wont Work
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
